3cf48377429ce60ffabcaf6450f98c50bd6dc156
Bashir Aminu Bello <75594157+bellonaut@users.noreply.github.com>
2026-02-03 01:56:08 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bellonaut.com/",
+  site: "https://bashir.bio",
   integrations: [
     sitemap(),
     robotsTxt({
